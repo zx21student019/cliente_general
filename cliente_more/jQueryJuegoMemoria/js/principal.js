aaeb43d369e4fc5baba0a66aa31adf55b708409c
more <zx21studentrcolegiovalledelmiro.es>
2023-01-23 13:58:36 +0100

    Memoria Primera carta

---
+++ cliente_more/jQueryJuegoMemoria/js/principal.js
@@ -0,0 +1,10 @@
+$(".carta1").click(voltear1);
+$(".carta2").click(voltear2);
+$(".carta3").click(voltear3);
+$(".carta4").click(voltear4);
+$(".carta5").click(voltear5);
+$(".carta6").click(voltear6);
+
+function voltear1(){
+    $(".carta1").css("background-image", "url(imagenes/muerte.jpg)");
+}
