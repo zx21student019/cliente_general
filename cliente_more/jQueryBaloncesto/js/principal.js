683f21c6c09f703b6b64f548ad39e6e3b15c68b9
more <zx21studentrcolegiovalledelmiro.es>
2023-01-25 10:45:09 +0100

    baloncesto empezado

---
+++ cliente_more/jQueryBaloncesto/js/principal.js
@@ -0,0 +1,12 @@
+// asociamos al boton btn la funcion registraPartido
+$("#btnER").click(registrapartido);
+$("#btnMF").click(modificaPartido);
+$("#btnDL").click(borraPartido);
+$("#btnLF").click(limpiaFormulario);
+
+
+//carga de datos inicail,directo de la BD a ala clave html
+function cargaInicial() {
+
+}
+function iniciarTabla
