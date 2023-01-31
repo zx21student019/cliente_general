a29336b4ef66b8a150b8e03c6d247fa68237f02b
more <zx21studentrcolegiovalledelmiro.es>
2023-01-31 11:41:33 +0100

    Basloncestoo

---
+++ cliente_more/jQueryBaloncesto/text.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Resultados (
+    ID int NOT NULL AUTO_INCREMENT,
+    EquipoLocal varchar(255) NOT NULL,
+    EquipoVisitante varchar(255) NOT NULL,
+    PuntosLocal int NOT NULL,
+    PuntosVisitante int NOT NULL,
+    PRIMARY KEY
+)
