47f03a261f0f671c2dc2c0ba8e84b3a6cf96b2e8
more <zx21studentrcolegiovalledelmiro.es>
2023-02-14 13:35:57 +0100

    api tiempo a falta de grafica

--- cliente_Erik/JqueryAjaxTiempo/js/principal.js
+++ cliente_Erik/JqueryAjaxTiempo/js/principal.js
@@ -5,15 +5,15 @@ let dias = [];
 let tempMax = [];
 let tempMin = [];
 
-function principal(){
+function principal() {
 	$.ajax({
 		url: "https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/28161/?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlcmljLmZyYW5jaXNjby5rbnVjaGVsQGdtYWlsLmNvbSIsImp0aSI6IjNjOGQ2NzE2LTQxNDAtNDg4OC04ZjhhLTY0MTZmYjVjN2QwNSIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNjc1MDYzODA3LCJ1c2VySWQiOiIzYzhkNjcxNi00MTQwLTQ4ODgtOGY4YS02NDE2ZmI1YzdkMDUiLCJyb2xlIjoiIn0.jQy_XMbhR1TxZUhZVLMjNkNXI8ZAbv00stIBSdBsSGE",
 	}).done(function (response) {
-	  	UrlDatos = response.datos;
-	  	$.ajax({
-	  		url:UrlDatos,
-	  	}).done(function (response){
-	  		let responseJSON = $.parseJSON(response)
+		UrlDatos = response.datos;
+		$.ajax({
+			url: UrlDatos,
+		}).done(function (response) {
+			let responseJSON = $.parseJSON(response)
 			console.log(responseJSON);
 			rellenarTabla(responseJSON);
 			crearGrafico(responseJSON);
@@ -21,29 +21,29 @@ function principal(){
 	});
 }
 
-function rellenarTabla(dat){
+function rellenarTabla(dat) {
 	let arrayDias = dat[0].prediccion.dia;
 	for (let index = 0; index < arrayDias.length; index++) {
 		let fecha = arrayDias[index].fecha
 
-		let fechaGuardar = new Date(fecha).getDate()+"/"+(new Date(fecha).getMonth()+1);
+		let fechaGuardar = new Date(fecha).getDate() + "/" + (new Date(fecha).getMonth() + 1);
 
 		dias.push(fechaGuardar);
 
-		const opciones = {weekday:'short', day:'numeric'}
+		const opciones = { weekday: 'short', day: 'numeric' }
 		$("#row_fecha").append(
-			`<td colspan="2">${new Date(fecha).toLocaleDateString('es-ES',opciones)}</td>`
+			`<td colspan="2">${new Date(fecha).toLocaleDateString('es-ES', opciones)}</td>`
 		)
 
 		let contador = 0;
 
-		while(arrayDias[index].estadoCielo[contador].value == ''){
-			contador ++;
+		while (arrayDias[index].estadoCielo[contador].value == '') {
+			contador++;
 		}
 
 		$('#row_tiempo').append(
 			`<td colspan="2"><img src="https://www.aemet.es/imagenes/png/estado_cielo/${arrayDias[index].estadoCielo[contador].value}_g.png"></td>`
-		);		
+		);
 
 		$("#row_temp_titulos").append(
 			`<td id="row_temp_titulo_min">Tª mínima</td>
@@ -60,7 +60,7 @@ function rellenarTabla(dat){
 	}
 }
 
-function crearGrafico(d){
+function crearGrafico(d) {
 
 	let grafLineas = $("#graficoLineas")
 
@@ -86,8 +86,8 @@ function crearGrafico(d){
 			pointHitRadius: 30,
 			pointBorderWidth: 2,
 			pointStyle: 'rectRounded'
-		  },
-		  {
+		},
+		{
 			label: "Tª mínima",
 			data: tempMin,
 			lineTension: 0,
@@ -102,16 +102,16 @@ function crearGrafico(d){
 			pointHitRadius: 30,
 			pointBorderWidth: 2,
 			pointStyle: 'rectRounded'
-		  }]
+		}]
 	};
 	var chartOptions = {
 		legend: {
-		  display: true,
-		  position: 'top',
-		  labels: {
-			boxWidth: 5,
-			fontColor: 'black'
-		  }
+			display: true,
+			position: 'top',
+			labels: {
+				boxWidth: 5,
+				fontColor: 'black'
+			}
 		}
 	};
 
--- cliente_more/tiempoApi/js/principal.js
+++ cliente_more/tiempoApi/js/principal.js
@@ -1,5 +1,8 @@
 let urlDatos
 let jsonResponse
+let dias = [];
+let tempMax = [];
+let tempMin = [];
 
 $(principal)
 
@@ -8,42 +11,81 @@ function principal() {
     $.ajax({
         "url": "https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/28161/?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtb3Jlc2VyZ2lvdmFsQGdtYWlsLmNvbSIsImp0aSI6ImYzMjE3YWJhLWViMTMtNDFiMS1iMzkzLWIzZjYxMmUyMTA1MCIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNjc1MjM2NjYyLCJ1c2VySWQiOiJmMzIxN2FiYS1lYjEzLTQxYjEtYjM5My1iM2Y2MTJlMjEwNTAiLCJyb2xlIjoiIn0.izGHILhTGnloR063P8rUPibOGMiiKxCopSJmGiwkFUk"
     }).done(function (response) {
-        console.log(response);
         urlDatos = response.datos
 
         $.ajax({
             "url": urlDatos
         }).done(function (response) {
-            console.log(response);
             jsonResponse = $.parseJSON(response)
+            estructuraTabla(jsonResponse);
         });
     });
-    divsDias()
+
 }
 
-function divsDias() {
-    for (var i = 0; i < 6; i++) {
-        let trDias = {
-            class: "tr"
-        }
-        let img = {
-            class: "imagenes",
-            id: i,
-            oculto: "si",
-            src: "imagenes/joker.jpg"
-        }
-        let trPorcentaje = {
-            class: "tr"
-        }
-        let trMinMax = {
-            class: "tr"
+function estructuraTabla(datos) {
+    let trDias = {
+        class: "trDias"
+    }
+    let trImg = {
+        class: "trImg"
+    }
+    let trPorcentaje = {
+        class: "trPorcentaje"
+    }
+    let trMin = {
+        class: "trMin"
+    }
+    let trMax = {
+        class: "trMax"
+    }
+
+    let tr1 = $("<tr>", trDias);
+    let tr2 = $("<tr>", trImg);
+    let tr3 = $("<tr>", trPorcentaje);
+    let tr4 = $("<tr>", trMax);
+    let tr5 = $("<tr>", trMin);
+
+
+    $(".tabla").append(tr1, tr2, tr3, tr4, tr5);
+
+    rellenarTabla(jsonResponse)
+}
+
+function rellenarTabla(datos) {
+    let arrayDias = datos[0].prediccion.dia;
+    for (let index = 0; index < arrayDias.length; index++) {
+        let fecha = arrayDias[index].fecha
+
+        let fechaGuardar = new Date(fecha).getDate() + "/" + (new Date(fecha).getMonth() + 1);
+
+        dias.push(fechaGuardar);
+
+        const opciones = { weekday: 'short', day: 'numeric' }
+        $(".trDias").append(
+            `<td>${new Date(fecha).toLocaleDateString('es-ES', opciones)}</td>`
+        )
+
+        let contador = 0;
+
+        while (arrayDias[index].estadoCielo[contador].value == '') {
+            contador++;
         }
 
-        let tr1 = $("<tr>", trDias);
-        let tr2 = $("<img>", img);
-        let tr3 = $("<tr>", trPorcentaje);
-        let tr4 = $("<tr>", trMinMax);
+        $('.trImg').append(
+            `<td><img src="https://www.aemet.es/imagenes/png/estado_cielo/${arrayDias[index].estadoCielo[contador].value}_g.png"></td>`
+        );
 
-        $(".tabla").append(tr1, tr2, tr3, tr4);
+        tempMax.push(arrayDias[index].temperatura.maxima);
+        tempMin.push(arrayDias[index].temperatura.minima);
+
+
+        $(".trMax").append(
+            `<td>máxima ${arrayDias[index].temperatura.maxima}º</td>`
+        )
+        $(".trMin").append(
+            `<td>mínima ${arrayDias[index].temperatura.minima}º</td>`
+        )
+
     }
 }
