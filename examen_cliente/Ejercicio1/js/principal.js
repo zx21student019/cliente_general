885defe5fe2423252b97592eaee3d087b9f3da16
zx21student019 <zx21student019@colegiovalledelmiro.es>
2023-03-06 20:20:11 +0100

    ds

--- examen_cliente/Ejercicio1/js/principal.js
+++ examen_cliente/Ejercicio1/js/principal.js
@@ -1,88 +1,36 @@
-$(principal)
+$('body').append($('<h2>').text('Ventas por mes'));
+$('body').append($('<h4>').text('Informe semanal'));
 
-function principal() {
+let tabla = $('<table>');
 
-    let idtbody = {
-        id: "cuerpotabla"
-    }
+let tr1 = $("<tr>");
+let tr2 = $("<tr>");
+let tr3 = $("<tr>");
+let tr4 = $("<tr>");
+let tr5 = $("<tr>");
+let tr6 = $("<tr>");
+let tr7 = $("<tr>");
 
-    let tr02 = {
-        class: "tr2"
-    }
-    let tr03 = {
-        class: "tr3"
-    }
-    let tr04 = {
-        class: "tr4"
-    }
-    let tr05 = {
-        class: "tr5"
-    }
-    let tr06 = {
-        class: "tr6"
-    }
-    let tr07 = {
-        class: "tr7"
-    }
+tr1.append($('<td>').text('Mes'));
+tr1.append($('<td>').text('Ventas'));
 
-    let h2 = $("<h2>");
-    let h4 = $("<h4>");
+tr2.append($('<td>').text('Enero'));
+tr2.append($('<td>').text('10325€'));
 
-    let table = $("<table>");
-    let thead = $("<thead>");
-    let tbody = $("<tbody>", idtbody);
+tr3.append($('<td>').text('Febreo'));
+tr3.append($('<td>').text('9188€'));
 
-    let tr1 = $("<tr>");
-    let tr2 = $("<tr>", tr02);
-    let tr3 = $("<tr>", tr03);
-    let tr4 = $("<tr>", tr04);
-    let tr5 = $("<tr>", tr05);
-    let tr6 = $("<tr>", tr06);
-    let tr7 = $("<tr>", tr07);
+tr4.append($('<td>').text('Marzo'));
+tr4.append($('<td>').text('11987€'));
 
-    $("body").append(h2, h4, table);
+tr5.append($('<td>').text('Abril'));
+tr5.append($('<td>').text('7625€'));
 
-    $("h2").append(
-        `Ventas por mes`
-    )
-    $("h4").append(
-        `Informe semestral`
-    )
-    $("table").append(
-        thead, tbody
-    )
-    $("thead").append(
-        tr1
-    )
-    $("tbody").append(
-        tr2, tr3, tr4, tr5, tr6, tr7
-    )
-    $("thead tr").append(
-        `<td>Mes</td>
-        <td>Ventas</td>`
-    )
-    $(".tr2").append(
-        `<td>Enero</td>
-        <td>10325€</td>`
-    )
-    $(".tr3").append(
-        `<td>Febreo</td>
-        <td>9188€</td>`
-    )
-    $(".tr4").append(
-        `<td>Marzo</td>
-        <td>11987€</td>`
-    )
-    $(".tr5").append(
-        `<td>Abril</td>
-        <td>7625€</td>`
-    )
-    $(".tr6").append(
-        `<td>Mayo</td>
-        <td>12967</td>`
-    )
-    $(".tr7").append(
-        `<td>Junio</td>
-        <td>11134</td>`
-    )
-}
+tr6.append($('<td>').text('Mayo'));
+tr6.append($('<td>').text('12967€'));
+
+tr7.append($('<td>').text('Junio'));
+tr7.append($('<td>').text('11134€'));
+
+$('table').append(tr1, tr2, tr3, tr4, tr5, tr6, tr7);
+$('body').append(tabla);
